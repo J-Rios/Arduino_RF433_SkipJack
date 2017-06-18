f16912b8557d12483d0e2d30a7f53137e7ac9ae2
J-Rios <jose_23991@hotmail.com>
2017-06-18 15:07:38 +0200

    Tx & Rx examples added (v 1.0)

---
+++ Rx/Rx.ino
@@ -0,0 +1,56 @@
+// Note (13/06/2017): Updated Manchester library from github repository fails 
+// when transmit data values above 767 (0x02FF), the receiver can't get the 
+// packages. To avoid that, use the Manchester sources provided in this example.
+
+#include "manchester.h"
+#include "skipjack.c"
+
+#define TX_DEVICE_ID 0x1B
+#define CMD 0xFF
+#define P_RX 4
+#define SKIPJ_KEY_SIZE 10
+#define SKIPJ_BLOCK_SIZE 8
+
+const uint8_t skipjack_key[SKIPJ_KEY_SIZE] = {0x00,0x99,0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11};
+
+uint8_t toDecodeData[SKIPJ_BLOCK_SIZE];
+
+void setup()
+{
+    Serial.begin(9600);
+    man.setupReceive(P_RX, MAN_1200);
+    man.beginReceiveArray(SKIPJ_BLOCK_SIZE, toDecodeData);
+}
+
+void loop()
+{
+    uint8_t id, cmd;
+    
+    if(man.receiveComplete())
+    {
+        skipjack_dec(toDecodeData, skipjack_key);
+        id = toDecodeData[0];
+        cmd = toDecodeData[1];
+
+        man.beginReceiveArray(SKIPJ_BLOCK_SIZE, toDecodeData);
+        
+        Serial.print("\nPackage received:\n");
+        Serial.print("ID - 0x");
+        Serial.print(id, HEX);
+        Serial.print(" , Data (command) - 0x");
+        Serial.print(cmd, HEX);
+        Serial.print("\n");
+        
+        if(id == TX_DEVICE_ID)
+        {
+            if(cmd == CMD)
+            {
+                Serial.print("Expected command from device [0x");
+                Serial.print(TX_DEVICE_ID, HEX);
+                Serial.print("] has been received\n");
+            }
+        }
+
+        Serial.print("\n");
+    }
+}
---
+++ Tx/Tx.ino
@@ -0,0 +1,45 @@
+// Note (13/06/2017): Updated Manchester library from github repository fails 
+// when transmit data values above 767 (0x02FF), the receiver can't get the 
+// packages. To avoid that, use the Manchester sources provided in this example.
+
+#include "manchester.h"
+#include "skipjack.c"
+
+#define MY_ID 0x1B // Decimal value: 175 - One Byte, range (0-255) | (0x00-0xFF)
+#define CMD 0xFF   // Decimal value: 255 - One Byte, range (0-255) | (0x00-0xFF)
+#define P_TX 9     // Transmission digital pin
+#define SKIPJ_KEY_SIZE 10
+#define SKIPJ_BLOCK_SIZE 8
+
+const uint8_t skipjack_key[SKIPJ_KEY_SIZE] = {0x00,0x99,0x88,0x77,0x66,0x55,0x44,0x33,0x22,0x11};
+
+
+uint8_t toEncodeData[SKIPJ_BLOCK_SIZE];
+
+void setup()
+{
+    Serial.begin(9600);
+    man.setupTransmit(P_TX, MAN_1200);
+}
+
+void loop()
+{
+    toEncodeData[0] = MY_ID;
+    toEncodeData[1] = CMD;
+    
+    Serial.print("\nPackage to send:\n");
+    Serial.print("ID - 0x");
+    Serial.print(toEncodeData[0], HEX);
+    Serial.print(" , Data (command) - 0x");
+    Serial.print(toEncodeData[1], HEX);
+    Serial.print("\n");
+
+    skipjack_enc(toEncodeData, skipjack_key);
+    man.transmitArray(SKIPJ_BLOCK_SIZE, toEncodeData);
+    
+    Serial.print("Package sent\n");
+    Serial.print("\n");
+    
+    delay(1000);
+}
+
